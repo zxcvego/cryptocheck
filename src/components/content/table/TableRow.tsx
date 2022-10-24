9d1270a46982b20eb3846e6cc719e868c9787bd4
zxcvego <zxcvego@gmail.com>
2022-10-24 23:21:21 +0200

    feat: add icons to appropriate coins

--- src/components/content/table/TableRow.tsx
+++ src/components/content/table/TableRow.tsx
@@ -2,6 +2,8 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart } from "@fortawesome/free-regular-svg-icons";
 import { CoinI } from "../CryptoTable";
 
+const COIN_ICON_URL = "https://assets.coincap.io/assets/icons/";
+
 const TableRow = ({ coin }: { coin: CoinI }) => {
 	return (
 		<>
@@ -10,7 +12,16 @@ const TableRow = ({ coin }: { coin: CoinI }) => {
 					<FontAwesomeIcon icon={faHeart} />
 				</td>
 				<td className="text-center w-3">{coin.rank}</td>
-				<td className="text-center">{coin.name}</td>
+				<td className="text-center">
+					<div className="flex justify-center items-center gap-2">
+						<img
+							src={`${COIN_ICON_URL}${coin.symbol.toLowerCase()}@2x.png`}
+							alt={`${coin.name} icon`}
+							className="w-6 h-6"
+						/>
+						{coin.name}
+					</div>
+				</td>
 				<td className="text-center">{coin.priceUsd}</td>
 				<td className="text-center">{coin.marketCapUsd}</td>
 			</tr>
