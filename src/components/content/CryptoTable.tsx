300831a59fc9784f4f91bec0a387100ea1523994
zxcvego <zxcvego@gmail.com>
2022-10-24 14:59:52 +0200

    fix: table structure

--- src/components/content/CryptoTable.tsx
+++ src/components/content/CryptoTable.tsx
@@ -3,15 +3,21 @@ import TableRow from "./table/TableRow";
 const CryptoTable = () => {
 	return (
 		<table className="container mx-auto bg-black text-white font-open-sans rounded-t-md">
-			<th className="leading-8 text-center pl-12">head 1</th>
-			<th className="leading-8 text-center pl-12">head 2</th>
-			<TableRow />
-			<TableRow />
-			<TableRow />
-			<TableRow />
-			<TableRow />
-			<TableRow />
-			<TableRow />
+			<thead>
+				<tr>
+					<th className="leading-8 text-center pl-12">head 1</th>
+					<th className="leading-8 text-center pl-12">head 2</th>
+				</tr>
+			</thead>
+			<tbody>
+				<TableRow />
+				<TableRow />
+				<TableRow />
+				<TableRow />
+				<TableRow />
+				<TableRow />
+				<TableRow />
+			</tbody>
 		</table>
 	);
 };
